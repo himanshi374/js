92856dd2803c0e49dcd3d587c58769159713271b
himanshi374 <131516748+himanshi374@users.noreply.github.com>
2023-09-28 12:28:40 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "himanshi511@gmail.com"
+var accountPassword = "12345"
+accountCity = "bhilwara"
+
+//accountId = 2 //not allowed 
+accountEmail = "panwarr987@gmail.com"
+accountPassword = "9999"
+accountCity = "jaipur"
+
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,3 @@
+"use strict";
+
+//alert( 3 + 3)//
