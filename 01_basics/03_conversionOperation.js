6c80d4de00a484845f6dc81d45eeac9ac6901a63
himanshi374 <131516748+himanshi374@users.noreply.github.com>
2023-09-30 10:25:41 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,30 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ************* operations ***************
+
+let value = 3
+let negValue = -value 
+console.log(negValue);
+
+let str1 = "himanshi"
+let str2 = " himanshi"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
