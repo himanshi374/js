0705dd0a7f5639fade5b56d3924de72854b198b4
himanshi374 <131516748+himanshi374@users.noreply.github.com>
2023-10-03 08:43:28 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,27 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString());
+//console.log(balance.toFixed(4));
+
+const otherNumber = 32.8566
+
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++ maths ++++++++++++++++++++++
+
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min)
