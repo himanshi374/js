12cee623fe79c026c3352094018a680826dd64df
himanshi374 <131516748+himanshi374@users.noreply.github.com>
2023-10-05 08:00:29 +0000

    obj de-structure and json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,7 +27,7 @@ const obj2 = {3: "a", 4:"b"}
 
 
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+//console.log(obj3);
 
 
 const users = [
@@ -46,6 +46,20 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+//console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
+//console.log(Object.keys(tinderUser));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "himanshi"
+}
+
+//course.courseInstructor
+
+const {courseInstructor} = course
+
+console.log(courseInstructor);
+
+
