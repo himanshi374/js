a7d4ec25e994926f1b34c455f0414ef202eafd02
himanshi374 <131516748+himanshi374@users.noreply.github.com>
2023-10-09 08:11:43 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -3,14 +3,57 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-     console.log("INNER ", a);
+     //console.log("INNER ", a);
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+//for (let i = 0; i < array.length; i++) {
+   // const element = array[i];
     
-}
+//}
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const username = "himanshi"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+
+
+//one()
+
+if (true) {
+    const username = "himanshi"
+    if(username==="himanshi"){
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+
+//++++++++++++++interesting++++++++++++//
+
+
+console.log(addone(5));
+
+function addone(num){
+return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
